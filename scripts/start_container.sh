5561f1b2331ca784f4ccc4dd015012887dbc9bfb
Prince Kumar Gautam <105717617+GautamPince@users.noreply.github.com>
2025-01-15 22:26:16 +0530

    update start container

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-set -e 
+set -e
 
 # Pull the docker image from docker hub
 docker pull priancegtm/myapp:latest
 
-# Run the docker image as a container 
-docker run -d -p 3000:5173 princegtm/myapp:latest
+# Run the docker image as a container
+docker run -d -p 3000:5173 priancegtm/myapp:latest
