dc7813330ee73813ea264924f9242541babbb73b
GautamPince <ramgautam9128@gmail.com>
2025-01-15 22:13:05 +0530

    correct typos

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -2,7 +2,7 @@
 set -e 
 
 # Pull the docker image from docker hub
-docker pull priancegtm/myapp
+docker pull priancegtm/myapp:latest
 
 # Run the docker image as a container 
-docker run -d -p 3000:5173 princegtm/myapp
+docker run -d -p 3000:5173 princegtm/myapp:latest
